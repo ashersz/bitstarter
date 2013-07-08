83a1a6497dadff61a47ba99dc6bf60c82fc324aa
Asher Szmulewicz <asher@szmulewicz.org>
2013-07-08 04:25:59 +0000

    read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = new Buffer(32);
+  buffer.write(fs.readFileSync("index.html"),"utf-8");
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
